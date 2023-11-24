c41e5478bfda71688c92ea43d280d7677a451e88
aduda <shemaduida@gmail.com>
2023-11-24 16:47:38 +0300

    Posts from users being followed

---
+++ src/components/Account/FollowingPosts.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { Container, Row, Col, Alert } from 'reactstrap';
+import { apiCalls } from '../../Data/Api';
+import { getFollowedUsers, getBlockedUsers } from './UserTrack';
+import Post from './Post'; 
+
+const Following = () => {
+  const [followingPosts, setFollowingPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchFollowingPosts = async () => {
+      try {
+        const followedUsers = getFollowedUsers();
+        const blockedUsers = getBlockedUsers();
+
+        const posts = await apiCalls.fetchPosts();
+
+        const filteredPosts = posts.filter(
+          (post) => followedUsers.includes(post.userId) && !blockedUsers.includes(post.userId)
+        );
+
+        setFollowingPosts(filteredPosts);
+      } catch (error) {
+        console.error('Error fetching following posts:', error);
+      }
+    };
+
+    fetchFollowingPosts();
+  }, []);
+
+  return (
+    <Container>
+      <h2 className="mb-4">Following Posts</h2>
+      {followingPosts.length > 0 ? (
+        followingPosts.map((post) => (
+          <Row key={post.id} className="mb-4">
+            <Col>
+              <Post post={post} />
+            </Col>
+          </Row>
+        ))
+      ) : (
+        <Alert color="info">No posts from followed users available.</Alert>
+      )}
+    </Container>
+  );
+};
+
+export default Following;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,6 +9,8 @@ import {
   Button,
   TabContent,
   TabPane,
+  CardHeader,
+  CardBody,
 } from "reactstrap";
 import classnames from "classnames";
 import Feed from "./Account/Feed";
@@ -16,6 +18,7 @@ import Users from "./Account/Users";
 import MyPosts from "./Account/MyPosts";
 import Profile from "./Auth/Profile";
 import Subscription from "../components/Account/subscription";
+import Following from "../components/Account/FollowingPosts";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -28,6 +31,15 @@ const HomePage = ({
   handleLogout,
   setShowLogin,
 }) => {
+  const tabs = [
+    { id: "feed", label: "Feed", component: <Feed isLoggedIn={isLoggedIn} isPremiumUser={isPremiumUser} remainingFreePosts={remainingFreePosts} /> },
+    { id: "Users", label: "Users", component: <Users isLoggedIn={isLoggedIn} isPremiumUser={isPremiumUser} /> },
+    { id: "myPosts", label: "Posts", component: <MyPosts isLoggedIn={isLoggedIn} isPremiumUser={isPremiumUser} /> },
+    { id: "profile", label: "Profile", component: <Profile /> },
+    { id: "payment", label: "Payment", component: <Subscription /> },
+    { id: "following", label: "Following", component: <Following isLoggedIn={isLoggedIn} /> },
+  ];
+
   return (
     <>
       <Card className="bg-gray-900 text-white p-8 mb-8 rounded-lg shadow-lg">
@@ -50,126 +62,46 @@ const HomePage = ({
       </Card>
 
       <Card className="p-4 bg-white mb-4 rounded-lg shadow-md">
-        <Nav pills className="flex items-center justify-center space-x-4">
-          <NavItem>
-            <NavLink
-              onClick={() => {
-                setActiveTab("feed");
-                setShowLogin(false);
-              }}
-              className={classnames("cursor-pointer text-lg font-bold", {
-                "text-blue-500": activeTab === "feed",
-              })}
-            >
-              Feed
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              onClick={() => {
-                setActiveTab("Users");
-                setShowLogin(false);
-              }}
-              className={classnames("cursor-pointer text-lg font-bold", {
-                "text-blue-500": activeTab === "Users",
-              })}
-            >
-              Users
-            </NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink
-              onClick={() => {
-                setActiveTab("myPosts");
-                setShowLogin(false);
-              }}
-              className={classnames("cursor-pointer text-lg font-bold", {
-                "text-blue-500": activeTab === "myPosts",
-              })}
-            >
-              Posts
-            </NavLink>
-          </NavItem>
-          {isLoggedIn && (
-            <>
-              <NavItem>
+        <CardHeader className="bg-gray-200">
+          <Nav pills className="flex items-center justify-center space-x-4">
+            {tabs.map(tab => (
+              <NavItem key={tab.id}>
                 <NavLink
                   onClick={() => {
-                    setActiveTab("profile");
+                    setActiveTab(tab.id);
                     setShowLogin(false);
                   }}
                   className={classnames("cursor-pointer text-lg font-bold", {
-                    "text-blue-500": activeTab === "profile",
+                    "text-blue-500": activeTab === tab.id,
                   })}
                 >
-                  Profile
+                  {tab.label}
                 </NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink
-                  onClick={() => {
-                    setActiveTab("payment");
-                    setShowLogin(false);
-                  }}
-                  className={classnames("cursor-pointer text-lg font-bold", {
-                    "text-blue-500": activeTab === "payment",
-                  })}
-                >
-                  Payment
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <Button
-                  onClick={() => (isLoggedIn ? handleLogout() : setShowLogin(true))}
-                  className={classnames("cursor-pointer text-lg font-bold", {
-                    "bg-red-500 text-white": isLoggedIn,
-                    "bg-blue-500 text-white": !isLoggedIn,
-                  })}
-                >
-                  {isLoggedIn ? "Logout" : "Login"}
-                </Button>
-              </NavItem>
-            </>
-          )}
-        </Nav>
+            ))}
+            <NavItem className="ml-auto">
+              <Button
+                onClick={() => (isLoggedIn ? handleLogout() : setShowLogin(true))}
+                className={classnames("cursor-pointer text-lg font-bold", {
+                  "bg-red-500 text-white": isLoggedIn,
+                  "bg-blue-500 text-white": !isLoggedIn,
+                })}
+              >
+                {isLoggedIn ? "Logout" : "Login"}
+              </Button>
+            </NavItem>
+          </Nav>
+        </CardHeader>
 
-
-        <div
-          className="scrollable-content overflow-y-auto overflow-x-hidden"
-          style={{ maxHeight: "1000px", height: "fit-content" }}
-        >
+        <CardBody className="scrollable-content overflow-y-auto overflow-x-hidden" style={{ maxHeight: "1000px", height: "fit-content" }}>
           <TabContent>
-            <TabPane>
-              {activeTab === "feed" && (
-                <Feed
-                  isLoggedIn={isLoggedIn}
-                  isPremiumUser={isPremiumUser}
-                  remainingFreePosts={remainingFreePosts}
-                />
-              )}
-            </TabPane>
-
-            <TabPane>
-              {activeTab === "Users" && (
-                <Users isLoggedIn={isLoggedIn} isPremiumUser={isPremiumUser} />
-              )}
-            </TabPane>
-
-            <TabPane>
-              {activeTab === "myPosts" && (
-                <MyPosts isLoggedIn={isLoggedIn} isPremiumUser={isPremiumUser} />
-              )}
-            </TabPane>
-
-            <TabPane>
-              {activeTab === "profile" && <Profile />}
-            </TabPane>
-
-            <TabPane>
-              {activeTab === "payment" && <Subscription />}
-            </TabPane>
+            {tabs.map(tab => (
+              <TabPane key={tab.id}>
+                {activeTab === tab.id && tab.component}
+              </TabPane>
+            ))}
           </TabContent>
-        </div>
+        </CardBody>
       </Card>
       <ToastContainer />
     </>
