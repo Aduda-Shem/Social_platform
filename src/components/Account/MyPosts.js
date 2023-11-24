34fd531ed2ce3c0ea59d0112ffe06fc85bd6ca9c
Aduda-Shem <shemaduida@gmail.com>
2023-11-24 09:35:30 +0300

    uerposts added chat

--- src/components/Account/MyPosts.js
+++ src/components/Account/MyPosts.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Alert, Button, Input, Card, CardBody } from 'reactstrap';
+import { Container, Row, Col, Alert, Button, Card, CardBody, Input } from 'reactstrap';
 import { apiCalls } from '../../Data/Api';
 import { getAuthToken } from '../../components/Auth/auth';
 import { FaTrash, FaArchive } from 'react-icons/fa';
@@ -75,42 +75,42 @@ const UserPosts = () => {
       <h2 className="mb-4">User Posts</h2>
       <Row className="mb-6">
         <Col>
-          <Card
-            className={`mb-4 fixed bottom-4 right-4 w-96 bg-white border-none rounded-lg shadow-lg ${
-              isChatOpen ? 'chat-open' : ''
-            }`}
-          >
+          <Card className={`mb-4 fixed bottom-4 right-4 w-96 bg-white border-none rounded-lg shadow-lg ${isChatOpen ? 'chat-open' : 'chat-closed'}`}>
             <CardBody>
-              <Input
-                type="text"
-                placeholder="Enter post title"
-                value={newTitle}
-                onChange={(e) => setNewTitle(e.target.value)}
-                className="mb-4 px-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              />
-              <Input
-                type="textarea"
-                placeholder="Enter post body"
-                value={newBody}
-                onChange={(e) => setNewBody(e.target.value)}
-                className="mb-4 px-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              />
               <Button
                 color="primary"
-                onClick={handleSend}
-                className="bg-blue-500 text-white hover:bg-blue-600 transition duration-300 px-6 py-2 rounded-full"
+                onClick={() => setIsChatOpen(!isChatOpen)}
+                className={`chat-toggle-btn ${isChatOpen ? 'close-chat-btn' : 'open-chat-btn'}`}
               >
-                Send
+                {isChatOpen ? 'Close Chat' : 'Open Chat'}
               </Button>
+              {isChatOpen && (
+                <>
+                  <Input
+                    type="text"
+                    placeholder="Enter post title"
+                    value={newTitle}
+                    onChange={(e) => setNewTitle(e.target.value)}
+                    className="mb-4 px-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                  />
+                  <Input
+                    type="textarea"
+                    placeholder="Enter post body"
+                    value={newBody}
+                    onChange={(e) => setNewBody(e.target.value)}
+                    className="mb-4 px-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                  />
+                  <Button
+                    color="primary"
+                    onClick={handleSend}
+                    className="bg-blue-500 text-white hover:bg-blue-600 transition duration-300 px-6 py-2 rounded-full"
+                  >
+                    Send
+                  </Button>
+                </>
+              )}
             </CardBody>
           </Card>
-          <Button
-            color="primary"
-            onClick={() => setIsChatOpen(!isChatOpen)}
-            className="chat-toggle-btn"
-          >
-            {isChatOpen ? 'Close Chat' : 'Open Chat'}
-          </Button>
         </Col>
       </Row>
 
